11e3c4f9b027ac483de518b5323d6d78e1eee3b2
Akilaxn <85438544+Akilan-droid@users.noreply.github.com>
2021-11-24 15:53:22 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,1 +1,27 @@
+/**
+ * @file feature.h
+ * @author Akilan B
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+#ifndef FEATURE_H_
+#define FEATURE_H_
+
+
+int BfRate();
+int LunchRate();
+int DinnerRate();
+int purchased();
+int choice();
+int Quantity();
+int Total();
+
+
+
+#define MACRO -99
+#endif
